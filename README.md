736f1649a9a8310576947944f57be2aca8c32d61
Endogen <081081@gmx.net>
2018-06-07 22:35:36 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,102 @@
+# TradeOgre Python API Wrapper
+
+## Preconditions
+In order to use this API wrapper you need to have API keys from [TradeOgre](https://tradeogre.com).
+
+Open `Account` --> `Settings` --> `API Keys`
+
+## Example usage
+
+#### Loading API Keys
+
+__From file__
+```python
+import TradeOgre
+
+trade_ogre = TradeOgre.API()
+trade_ogre.load_key('TradeOgre.key')
+```
+In this case the key must be on the first lineand the secret must be on the second line
+
+__As direct input in class__
+```python
+import TradeOgre
+
+trade_ogre = TradeOgre.API(key=some_key, secret=some_secret)
+```
+
+__As direct input in method__
+```python
+import TradeOgre
+
+trade_ogre = TradeOgre.API()
+trade_ogre.balances(key=some_key, secret=same_currency)
+```
+
+## Usage
+
+#### Markets
+Retrieve a listing of all markets
+```
+trade_ogre.markets()
+```
+
+#### Orders
+Retrieve the current order book for a market
+```
+trade_ogre.orders('BTC-XMR')
+```
+
+#### Ticker
+Retrieve the ticker for a market
+```
+trade_ogre.ticker('BTC-XMR')
+```
+
+#### History
+Retrieve the history of the last trades on a market
+```
+trade_ogre.history('BTC-XTL')
+```
+
+#### Balance
+Get the balance of a specific currency
+```
+trade_ogre.balance('BTC')
+```
+
+#### Balances
+Retrieve all balances for your account
+```
+trade_ogre.balance('BTC')
+```
+
+#### Buy
+Submit a buy order to the order book for a market
+```
+trade_ogre.buy('BTC-XMR', '10', '0.0123')
+```
+
+#### Sell
+Submit a sell order to the order book for a market
+```
+trade_ogre.sell('BTC-XMR', '10', '0.0123')
+```
+
+#### Order
+Retrieve information about a specific order
+```
+trade_ogre.order("BTC-XMR")
+```
+
+#### Orders
+Retrieve the active orders under your account
+```
+trade_ogre.orders('BTC-XMR')
+```
+
+#### Cancel
+Cancel an order on the order book
+```
+trade_ogre.cancel('a40ac710-8dc5-b5a8-aa69-389715197b14')
+```
---
+++ TradeOgre.py
@@ -0,0 +1,296 @@
+import requests
+
+
+class API(object):
+    """ Maintains a single session between this machine and TradeOgre.
+
+    Specifying a key/secret pair is optional. If not specified, key and
+    secret must be specified at the called method.
+
+    Query responses, as received by :py:mod:`requests`, are retained
+    as attribute :py:attr:`response` of this object. It is overwritten
+    on each query.
+
+    """
+    def __init__(self, key=None, secret=None):
+        """ Create an object with authentication information.
+
+        :param key: (optional) key identifier for queries to the API
+        :type key: str
+
+        :param secret: (optional) actual private key used to sign messages
+        :type secret: str
+
+        :returns: None
+
+        """
+        self.key = key
+        self.secret = secret
+        self.uri = 'https://tradeogre.com/api/v1'
+        self.response = None
+        return
+
+    def load_key(self, path):
+        """ Load key and secret from file.
+        Expected file format is key and secret on separate lines.
+
+        :param path: path to keyfile
+        :type path: str
+
+        :returns: None
+
+        """
+        with open(path, 'r') as f:
+            self.key = f.readline().strip()
+            self.secret = f.readline().strip()
+        return
+
+    def markets(self):
+        """ Retrieve a listing of all markets and basic information
+        including current price, volume, high, low, bid and ask.
+
+        :returns: :py:meth:`requests.Response.json`-deserialised Python object
+
+        """
+        self.response = requests.get(self.uri + '/markets').json()
+        return self.response
+
+    def orders(self, market):
+        """ Retrieve the current order book for a market such as 'BTC-XMR'.
+
+        :param market: market such as 'BTC-XMR'
+        :type market: str
+
+        :returns: :py:meth:`requests.Response.json`-deserialised Python object
+
+        """
+        self.response = requests.get(self.uri + '/orders/' + market).json()
+        return self.response
+
+    def ticker(self, market):
+        """ Retrieve the ticker for a market such as 'BTC-XMR', volume,
+        high, and low are in the last 24 hours, initial price is the
+        price from 24 hours ago.
+
+        :param market: market such as 'BTC-XMR'
+        :type market: str
+
+        :returns: :py:meth:`requests.Response.json`-deserialised Python object
+
+        """
+        self.response = requests.get(self.uri + '/ticker/' + market).json()
+        return self.response
+
+    def history(self, market):
+        """ Retrieve the history of the last trades on {market} limited to 100
+        of the most recent trades. The date is a Unix UTC timestamp.
+
+        :param market: market such as 'BTC-XMR'
+        :type market: str
+
+        :returns: :py:meth:`requests.Response.json`-deserialised Python object
+
+        """
+        self.response = requests.get(self.uri + '/history/' + market).json()
+        return self.response
+
+    def balance(self, currency, key=None, secret=None):
+        """ Get the balance of a specific currency for you account. The currency
+        field is required, such as 'BTC'. The total balance is returned and the
+        available balance is what can be used in orders or withdrawn.
+
+        :param currency: currency such as 'BTC'
+        :type currency: str
+
+        :param key: key identifier
+        :type key: str
+
+        :param secret: actual private key
+        :type secret: str
+
+        :returns: :py:meth:`requests.Response.json`-deserialised Python object
+
+        """
+        if key is None or secret is None:
+            key = self.key
+            secret = self.secret
+
+        if key is None or secret is None:
+            raise Exception('Either key or secret is not set! (Use `load_key()`.')
+
+        data = {"currency": currency}
+        self.response = requests.post(self.uri + '/account/balance', data=data, auth=(key, secret)).json()
+        return self.response
+
+    def balances(self, key=None, secret=None):
+        """ Retrieve all balances for your account.
+
+        :returns: :py:meth:`requests.Response.json`-deserialised Python object
+
+        """
+        if key is None or secret is None:
+            key = self.key
+            secret = self.secret
+
+        if key is None or secret is None:
+            raise Exception('Either key or secret is not set! (Use `load_key()`.')
+
+        self.response = requests.get(self.uri + '/account/balances', auth=(key, secret)).json()
+        return self.response
+
+    def buy(self, market, qty, price, key=None, secret=None):
+        """ Submit a buy order to the order book for a market. The success status
+        will be false if there is an error, and error will contain the error message.
+        Your available buy and sell balance for the market will be returned if
+        successful. If your order is successful but not fully fulfilled, the order
+        is placed onto the order book and you will receive a uuid for the order.
+
+        :param market: market such as 'BTC-XMR'
+        :type market: str
+
+        :param qty: quantity to buy
+        :type qty: str
+
+        :param price: price to buy for
+        :type price: str
+
+        :param key: key identifier
+        :type key: str
+
+        :param secret: actual private key
+        :type secret: str
+
+        :returns: :py:meth:`requests.Response.json`-deserialised Python object
+
+        """
+        if key is None or secret is None:
+            key = self.key
+            secret = self.secret
+
+        if key is None or secret is None:
+            raise Exception('Either key or secret is not set! (Use `load_key()`.')
+
+        data = {"market": market, "quantity": qty, "price": price}
+        self.response = requests.post(self.uri + '/order/buy', data=data, auth=(key, secret)).json()
+        return self.response
+
+    def sell(self, market, qty, price, key=None, secret=None):
+        """ Submit a sell order to the order book for a market. The success status
+        will be false if there is an error, and error will contain the error
+        message. Your available buy and sell balance for the market will be returned
+        if successful. If your order is successful but not fully fulfilled, the order
+        is placed onto the order book and you will receive a uuid for the order.
+
+        :param market: market such as 'BTC-XMR'
+        :type market: str
+
+        :param qty: quantity to sell
+        :type qty: str
+
+        :param price: price to sell for
+        :type price: str
+
+        :param key: key identifier
+        :type key: str
+
+        :param secret: actual private key
+        :type secret: str
+
+        :returns: :py:meth:`requests.Response.json`-deserialised Python object
+
+        """
+        if key is None or secret is None:
+            key = self.key
+            secret = self.secret
+
+        if key is None or secret is None:
+            raise Exception('Either key or secret is not set! (Use `load_key()`.')
+
+        data = {"market": market, "quantity": qty, "price": price}
+        self.response = requests.post(self.uri + '/order/sell', data=data, auth=(key, secret)).json()
+        return self.response
+
+    def order(self, uuid, key=None, secret=None):
+        """ Retrieve information about a specific order by the
+        uuid of the order. Date is a Unix UTC timestamp.
+
+        :param uuid: ID of order
+        :type uuid: str
+
+        :param key: key identifier
+        :type key: str
+
+        :param secret: actual private key
+        :type secret: str
+
+        :returns: :py:meth:`requests.Response.json`-deserialised Python object
+
+        """
+        if key is None or secret is None:
+            key = self.key
+            secret = self.secret
+
+        if key is None or secret is None:
+            raise Exception('Either key or secret is not set! (Use `load_key()`.')
+
+        self.response = requests.get(self.uri + '/account/order/' + uuid, auth=(key, secret)).json()
+        return self.response
+
+    def orders(self, market=None, key=None, secret=None):
+        """ Retrieve the active orders under your account. The market
+        field is optional, and leaving it out will return all orders
+        in every market. date is a Unix UTC timestamp.
+
+        :param market: (optional) market to list orders from
+        :type market: str
+
+        :param key: key identifier
+        :type key: str
+
+        :param secret: actual private key
+        :type secret: str
+
+        :returns: :py:meth:`requests.Response.json`-deserialised Python object
+
+        """
+        if key is None or secret is None:
+            key = self.key
+            secret = self.secret
+
+        if key is None or secret is None:
+            raise Exception('Either key or secret is not set! (Use `load_key()`.')
+
+        if market is None:
+            market = ''
+
+        data = {"market": market}
+        self.response = requests.post(self.uri + '/account/orders', data=data, auth=(key, secret)).json()
+        return self.response
+
+    def cancel(self, uuid, key=None, secret=None):
+        """ Cancel an order on the order book based on the order uuid.
+        The uuid parameter can also be set to all and all of your
+        orders will be cancelled across all markets.
+
+        :param uuid: ID of order to cancel
+        :type uuid: str
+
+        :param key: key identifier
+        :type key: str
+
+        :param secret: actual private key
+        :type secret: str
+
+        :returns: :py:meth:`requests.Response.json`-deserialised Python object
+
+        """
+        if key is None or secret is None:
+            key = self.key
+            secret = self.secret
+
+        if key is None or secret is None:
+            raise Exception('Either key or secret is not set! (Use `load_key()`.')
+
+        data = {"uuid": uuid}
+        self.response = requests.post(self.uri + '/order/cancel', data=data, auth=(key, secret)).json()
+        return self.response
